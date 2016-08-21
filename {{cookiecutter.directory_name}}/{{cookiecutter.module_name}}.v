ef5068ea8b4aaf21bb35e39a99aa386b45bc294e
Thomas Fors <tom@fors.net>
2016-08-21 13:55:14 -0500

    Added Avalon-MM and Avalon-ST interface options

--- {{cookiecutter.directory_name}}/{{cookiecutter.module_name}}.v
+++ {{cookiecutter.directory_name}}/{{cookiecutter.module_name}}.v
@@ -1,5 +1,5 @@
 // {{cookiecutter.module_name}}.v
-// 
+//
 // {{cookiecutter.email}}, {{cookiecutter.date}}
 //
 // Purpose:
@@ -10,15 +10,84 @@ module {{cookiecutter.module_name}} (
 	input             clk,
 	input             reset,
 
+	{%- if cookiecutter.include_avalon_mm_interface == 'True' %}
+
+	// Avalon-MM Interface
+	input       [1:0] address,
+	input             write,
+	input      [31:0] writedata,
+	input             read,
+	output reg [31:0] readdata = 0,
+	{%- endif %}
+
+	{%- if cookiecutter.include_avalon_st_interface == 'True' %}
+
+	// Avalon-ST Sink Interface
+	input       [7:0] asi_data,
+	input             asi_valid,
+
+	// Avalon-ST Source Interface
+	output reg  [7:0] aso_data = 0,
+	output reg        aso_valid = 0,
+	{%- endif %}
+
 	output reg [31:0] counter
 );
+
+{%- if cookiecutter.include_avalon_mm_interface == 'True' %}
+
+reg [31:0] csr_reg [0:3];
+wire enable_counter;
+
+assign enable_counter = csr_reg[0][31];
+{%- endif %}
 
 always @(posedge clk) begin
 	if (reset == 1) begin
 		counter <= 0;
 	end else begin
+		{%- if cookiecutter.include_avalon_mm_interface == 'True' %}
+		if (enable_counter == 1) begin
+			counter <= counter + 1;
+		end
+
+		{%- else %}
 		counter <= counter + 1;
+		{%- endif %}
 	end
+
+{%- if cookiecutter.include_avalon_st_interface == 'True' %}
+
+	// Avalon-ST Processing
+	aso_valid <= 0;
+	if (asi_valid == 1) begin
+		aso_valid <= 1;
+		aso_data <= ~asi_data;
+	end
+{%- endif %}
+
+{%- if cookiecutter.include_avalon_mm_interface == 'True' %}
+
+	// Avalon-MM Read Interface
+	if (read == 1) begin
+		case (address)
+			0:	readdata <= csr_reg[0];
+			1:	readdata <= csr_reg[1];
+			2:	readdata <= csr_reg[2];
+			3:	readdata <= csr_reg[3];
+		endcase
+	end
+
+	// Avalon-MM Write Interface
+	if (write == 1) begin
+		case (address)
+			0:	csr_reg[0] <= writedata;
+			1:	csr_reg[1] <= writedata;
+			2:	csr_reg[2] <= writedata;
+			3:	csr_reg[3] <= writedata;
+		endcase
+	end
+{%- endif %}
 end
 
 endmodule
--- {{cookiecutter.directory_name}}/{{cookiecutter.module_name}}_tb.v
+++ {{cookiecutter.directory_name}}/{{cookiecutter.module_name}}_tb.v
@@ -5,9 +5,24 @@ module {{cookiecutter.module_name}}_tb;
 reg clock = 0;
 reg reset = 0;
 
+{%- if cookiecutter.include_avalon_mm_interface == 'True' %}
+
+reg [1:0] address = 0;
+reg read = 0;
+wire [31:0] readdata;
+reg write = 0;
+reg [31:0] writedata = 0;
+{%- endif %}
+
+{%- if cookiecutter.include_avalon_st_interface == 'True' %}
+
+reg [7:0] st_data = 0;
+reg st_valid = 0;
+{%- endif %}
+
 // -------------------------------------------------------
 // CLOCK GENERATION
-// ------------------------------------------------------- 
+// -------------------------------------------------------
 
 always begin
 	clock=0; #4;  // 125 MHz
@@ -16,15 +31,19 @@ end
 
 // -------------------------------------------------------
 // TERMINATION
-// ------------------------------------------------------- 
+// -------------------------------------------------------
 
 initial begin
-	#100 $finish;	
+	{%- if cookiecutter.include_avalon_mm_interface == 'True' %}
+	#2000 $finish;
+	{%- else %}
+	#100 $finish;
+	{%- endif %}
 end
 
 // -------------------------------------------------------
 // INITIALIZATION
-// ------------------------------------------------------- 
+// -------------------------------------------------------
 
 initial begin
 	if ($test$plusargs("vcd")) begin
@@ -35,7 +54,7 @@ end
 
 // -------------------------------------------------------
 // TEST CASES
-// ------------------------------------------------------- 
+// -------------------------------------------------------
 
 initial begin
 	// Provide dut stimulus here
@@ -43,9 +62,49 @@ initial begin
 	#8 reset = 0;
 end
 
+{%- if cookiecutter.include_avalon_mm_interface == 'True' %}
+
+initial begin
+	#1000 @(negedge clock)
+	address = 0;
+	writedata = 32'h80000000;
+	write = 1;
+	#8
+	write = 0;
+end
+{%- endif %}
+
+{%- if cookiecutter.include_avalon_st_interface == 'True' %}
+
+always begin
+	@(posedge clock)
+	st_data <= st_data + 1;
+	st_valid <= ~st_valid;
+end
+{%- endif %}
+
+
 {{cookiecutter.module_name}} dut (
 	.clk(clock),
 	.reset(reset),
+
+	{%- if cookiecutter.include_avalon_mm_interface == 'True' %}
+
+	.address(address),
+	.read(read),
+	.readdata(readdata),
+	.write(write),
+	.writedata(writedata),
+	{%- endif %}
+
+	{%- if cookiecutter.include_avalon_st_interface == 'True' %}
+
+	.asi_data(st_data),
+	.asi_valid(st_valid),
+
+	.aso_data(),
+	.aso_valid(),
+	{%- endif %}
 
 	.counter()
 );
